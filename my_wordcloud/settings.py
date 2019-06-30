40a8ef2703bdd37e7c8c4fc55a6d9de4dfd077dc
AaronF <aaronfarntrog@gmail.com>
2019-06-30 17:11:25 -0400

    changed os.get.environ()

--- my_wordcloud/settings.py
+++ my_wordcloud/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('WORDCLOUD_SK')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_VALUE')
+DEBUG = os.environ.get('DEBUG_VALUE', False)
 
 ALLOWED_HOSTS = ['*']
 
