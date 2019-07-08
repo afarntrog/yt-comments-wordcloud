bc7295c6bb7bb1dcf51a71ddd32fc21166314f81
AaronF <aaronfarntrog@gmail.com>
2019-07-08 10:50:12 -0400

    added date to admin

--- wowordcloud_app/admin.py
+++ wowordcloud_app/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(YoutubeUrl)
+
+class YoutubeUrlAdmin(admin.ModelAdmin):
+    readonly_fields = ('date',)
+
+admin.site.register(YoutubeUrl, YoutubeUrlAdmin)
+# admin.site.register(YoutubeUrl)
