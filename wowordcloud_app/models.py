7654e154ce47781fe89c05aff84fa4411956dd4d
AaronF <aaronfarntrog@gmail.com>
2019-07-08 10:38:43 -0400

    added date model filed

--- wowordcloud_app/models.py
+++ wowordcloud_app/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
 # Create your models here.
 class YoutubeUrl(models.Model):
     yt_url = models.CharField(max_length=200)
+    date = models.DateField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.yt_url[:50]
